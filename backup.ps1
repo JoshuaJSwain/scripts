e7ccabb25fbfa44722338827c484cb7367402c9e
joshua swain <joshua.j.swain@gmail.com>
2024-03-06 11:20:09 -0500

    backup

--- backup.ps1
+++ backup.ps1
@@ -1,9 +1,10 @@
 
 # Note the / slash is explicit. If the gs bucket path is \ it will copy every time and not handle crc rsync checks
 $folders = @'
-c:/users/joshu/Documents
-c:/users/joshu/Desktop
-c:/users/joshu/Pictures
+c:/users/joshu/documents
+c:/users/joshu/desktop
+c:/users/joshu/pictures
+c:/users\joshu/"my drive"
 '@ -split '\n'
 
 $folders.count
@@ -11,7 +12,7 @@ $folders.count
 #c:/users/joshu/Music
 
 foreach ($folder in $folders) {
-    gsutil -m rsync -r -c -x '^desktop.ini' $($folder) "gs://jswain-backup-pro8/$($folder)"
+    gsutil -m rsync -d -r -c -x '^desktop.ini' $($folder) "gs://jswain-backup-pro8/$($folder)"
     
 }
 
